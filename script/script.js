bfa71c52e30e3e1c06e2339d21c53442ae4a63b2
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-04-24 18:02:06 +0900

    메인 퍼블리싱 중

--- script/script.js
+++ script/script.js
@@ -4,7 +4,46 @@ document.addEventListener("DOMContentLoaded", function(e){
     scrollBottom();
     applyBottomBtn();
     applyRightBtn();
+    interiorArti2_slide();
+    youtubeBackground();
 })
+
+const youtubeBackground = () => {
+    $('[data-vbg]').youtube_background(); // 실행
+}
+
+const interiorArti2_slide = () => {
+    var swiper = new Swiper(".main .interiorArti2 .slideWrap .swiper", {
+        slidesPerView: 4,
+        spaceBetween: 24,
+        slidesPerGroupSkip: 4,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        loop : true,
+        pagination: {
+          el: ".main .interiorArti2 .slideWrap .swiper-pagination",
+          clickable: true,
+        },
+
+        breakpoints: {
+            1250: {
+              slidesPerView: 3,
+              spaceBetween: 24,
+            },
+            900: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            600: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+        },
+
+    });
+}
 
 const applyRightBtn = () => {
     $('.RightFloatingSec .showHideBtn').click(function(){
